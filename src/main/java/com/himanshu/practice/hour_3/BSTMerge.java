5018e9d81d45d3692246a0b6f3fbf9d6af4490aa
Himanshu Bhardwaj <himanshubhardwaj@Himanshus-MacBook.local>
2018-06-10 22:53:51 +0530

    minor changes

---
+++ src/main/java/com/himanshu/practice/hour_3/BSTMerge.java
@@ -0,0 +1,44 @@
+package com.himanshu.practice.hour_3;
+
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by himanshubhardwaj on 10/06/18.
+ * 12:52 am
+ */
+public class BSTMerge {
+}
+
+
+class Node {
+    int value;
+    Node left;
+    Node right;
+
+    //a is heaver side
+    public static Node merge(Node node1, Node node2) {
+        if (node1 == null && node2 == null) {
+            return null;
+        }
+        if (node1 != null && node2 == null) {
+            return node1;
+        }
+        if (node1 == null && node2 != null) {
+            return node2;
+        }
+
+        //now both are not null
+
+        if(node1.value < node2.value) {
+            node1.right = merge(node1.right, node2);
+        }
+
+
+        return null;
+
+    }
+
+    public static Node insert(Node a, Node b) {
+        return null;
+    }
+}
