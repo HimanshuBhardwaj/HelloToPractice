9221be52b1f76da80580edfd7624a34fa12787ce
Himanshu Bhardwaj <himanshubhardwaj@Himanshus-MacBook.local>
2018-07-05 17:20:28 +0530

    minor changes

--- src/main/java/com/himanshu/practice/july5/Sorting.java
+++ src/main/java/com/himanshu/practice/july5/Sorting.java
@@ -3,11 +3,13 @@ package com.himanshu.practice.july5;
 /**
  * Created by himanshubhardwaj on 05/07/18.
  * Mergesort:  3:51 --> 4:05
- * Quick Sort:  4:07 -->
+ * Quick Sort, insertionSort:  4:07 --> 4:31
+ * rotationPivot 4:32 -->5:12
+ * binary search 5:12 --> 5:20
  */
 public class Sorting {
     public static void main(String[] args) {
-        int arr[] = new int[19];
+        int arr[] = new int[23];
 
 
         for (int i = 0; i < arr.length; i++) {
@@ -33,6 +35,28 @@ public class Sorting {
         }
         System.out.println();
 
+        int[] arrInsert = arr.clone();
+        InsertionSort.sort(arrInsert);
+        System.out.println();
+        for (int i = 0; i < arrInsert.length; i++) {
+
+            System.out.print(arrInsert[i] + " ");
+        }
+        System.out.println();
+        System.out.println("Rotation time");
+
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i * i;
+        }
+//        arr[0] = 0;
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.println();
+        System.out.println(Array.rotationPoint(arr));
+        System.out.println(Array.binarySearch(0,arr.length-1,arr,121));
 
     }
 
@@ -128,9 +152,86 @@ class QuickSort {
         sortHelper(less + 2, end, arr);
     }
 
-    private static void swap(int pos1, int pos2, int[] arr) {
+    public static void swap(int pos1, int pos2, int[] arr) {
         int temp = arr[pos1];
         arr[pos1] = arr[pos2];
         arr[pos2] = temp;
+    }
+}
+
+class InsertionSort {
+    public static void sort(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[i]) {
+                    QuickSort.swap(i, j, arr);
+                }
+            }
+        }
+    }
+}
+
+class Array {
+    public static int rotationPoint(int arr[]) {
+        return rotationPointHelper(0, arr.length - 1, arr);
+    }
+
+    //rotation point is always inside the segment and in case of equality it is start
+    private static int rotationPointHelper(int start, int end, int[] arr) {
+        if (start > end || start == end) {
+            return 0;
+        }
+        if (start == end - 1) {
+            if (arr[end] < arr[start]) {
+                return end;
+            } else {
+                return start;
+            }
+        }
+
+        int mid = start + (end - start) / 2;
+
+        if (mid < end) {
+            if (arr[mid + 1] < arr[mid]) {
+                return mid + 1;
+            }
+        }
+        if (mid > start) {
+            if (arr[mid] < arr[mid - 1]) {
+                return mid;
+            }
+        }
+        if (arr[mid] > arr[end]) {
+            return rotationPointHelper(mid + 1, end, arr);
+        } else {
+            return rotationPointHelper(start, mid - 1, arr);
+        }
+    }
+
+
+    public static int binarySearch(int start, int end, int[] arr, int element) {
+        if (start > end) {
+            return -1;
+        }
+
+        if (start == end) {
+            if (arr[start] == element) {
+                return start;
+            } else {
+                return -1;
+            }
+        }
+
+        int mid = start + (end - start) / 2;
+
+        if (arr[mid] == element) {
+            return mid;
+        }
+        if (arr[mid] < element) {
+            return binarySearch(mid + 1, end, arr, element);
+        } else {
+            return binarySearch(start, mid - 1, arr, element);
+        }
+
     }
 }
