a0d7e3d0c43498747409024fc90b9063a670852a
Himanshu Bhardwaj <himanshubhardwaj@Himanshus-MacBook.local>
2018-07-07 17:34:08 +0530

    working word wrapping

--- src/main/java/com/himanshu/practice/july5/hour8/StringWrapping.java
+++ src/main/java/com/himanshu/practice/july5/hour8/StringWrapping.java
@@ -5,17 +5,25 @@ import java.util.Scanner;
 
 /**
  * Created by himanshubhardwaj on 06/07/18.
- * TODO: Not working
+ * working :-)
  */
 public class StringWrapping {
     static HashMap<Integer, Integer> map = new HashMap<>();
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int l = Integer.parseInt(sc.nextLine());
-        String str = sc.nextLine();
+        //Scanner sc = new Scanner(System.in);
+        //int l = Integer.parseInt(sc.nextLine());
+        //String str = sc.nextLine();
+        int l = 9;
+        String str = "I am    Himansu Bhardwaj. An idiot, a Gadha, moron";
         str.trim();
         System.out.println(minLines(0, str, l));
+
+
+        str = "I am Himansu Bhardwaj";
+        str.trim();
+        System.out.println(minLines(0, str, l));
+
     }
 
     private static int minLines(int start, String str, int wrapLength) {
@@ -28,10 +36,8 @@ public class StringWrapping {
             return map.get(start);
         }
 
-        System.out.println(start + "," + (str.length() - start));
         if ((str.length() - start) <= wrapLength) {
             map.put(start, 1);
-            System.out.println(start + "...reaching here");
             return 1;
         }
 
@@ -39,16 +45,23 @@ public class StringWrapping {
         int minLine = Integer.MAX_VALUE - 1000;
 
 
-        for (int i = start + 1; i <= Math.min(start + wrapLength - 1, str.length() - 1); i++) {
-            if (str.charAt(i) == ' ') {
-                minLine = Math.min(minLine, minLines(i, str, wrapLength));
+        for (int i = start; i <= Math.min(start + wrapLength - 1, str.length() - 1); i++) {
+            if (couldBreakedHere(i, str)) {
                 minLine = Math.min(minLine, minLines(i + 1, str, wrapLength));
             }
         }
         minLine++;
-        System.out.println(start + "\t" + minLine);
         map.put(start, minLine);
         return minLine;
+    }
+
+    private static boolean couldBreakedHere(int i, String str) {
+        if ((str.charAt(i) == ' ') || (i < (str.length() - 1) && str.charAt(i + 1) == ' ') || i == (str.length() - 1)) {
+            return true;
+        } else {
+            return false;
+        }
+
     }
 
 }
