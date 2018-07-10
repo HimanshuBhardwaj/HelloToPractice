2fef49132ea4ac64ad67b7c469b8bb5b32dd1f85
Himanshu Bhardwaj <himanshubhardwaj169@gmail.com>
2018-07-10 10:14:39 +0530

    two questions

---
+++ src/main/java/com/himanshu/practice/july10/Tournament.java
@@ -0,0 +1,66 @@
+package com.himanshu.practice.july10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by himanshubhardwaj on 10/07/18.
+ * Algo: Transitive Closure
+ * Submission: https://codeforces.com/contest/27/submission/40157471
+ */
+public class Tournament {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int adj[][] = new int[n][n];
+        int count[] = new int[n];
+
+        for (int i = 1; i < ((n - 1) * n) / 2; i++) {
+            String string[] = br.readLine().split(" ");
+            int source = Integer.parseInt(string[0]);
+            int destination = Integer.parseInt(string[1]);
+            source--;
+            destination--;
+            count[source]++;
+            count[destination]++;
+            adj[source][destination] = 1;
+            //System.out.println(source + " " + destination);
+        }
+
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (adj[i][k] == 1 && adj[k][j] == 1) {
+                        adj[i][j] = 1;
+                    }
+                }
+            }
+        }
+
+//        for (int i = 0; i < n; i++) {
+//            for (int j = 0; j < n; j++) {
+//                System.out.print(adj[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+//        System.out.println();
+        int exp = n - 1;
+        int souDest[] = new int[2];
+        int pos = 0;
+        for (int i = 0; i < n; i++) {
+            if (count[i] != exp) {
+                souDest[pos] = i;
+                pos++;
+            }
+        }
+
+        if (adj[souDest[0]][souDest[1]] == 1) {
+            System.out.println((souDest[0] + 1) + " " + (souDest[1] + 1));
+        } else {
+            System.out.println((souDest[1] + 1) + " " + (souDest[0] + 1));
+        }
+
+    }
+}
