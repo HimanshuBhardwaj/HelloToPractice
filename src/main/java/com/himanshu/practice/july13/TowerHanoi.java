37b608aa15558d22f15f40182f8fef63910be64b
Himanshu Bhardwaj <himanshubhardwaj169@gmail.com>
2018-07-13 03:49:28 +0530

    minor changes

---
+++ src/main/java/com/himanshu/practice/july13/TowerHanoi.java
@@ -0,0 +1,32 @@
+package com.himanshu.practice.july13;
+
+/**
+ * Created by himanshubhardwaj on 13/07/18.
+ * Tower of Hanoi recursion
+ */
+public class TowerHanoi {
+    public static void main(String[] args) {
+        towerOfHanoi('A', 'B', 'C', 3);
+
+    }
+
+    public static void towerOfHanoi(char source, char intermediate, char destination, int num) {
+        if (num == 0) {
+            return;
+        }
+        if (num == 1) {
+            System.out.println(source + " --> " + destination);
+            return;
+        }
+
+        if (num == 2) {
+            System.out.println(source + " --> " + intermediate);
+            System.out.println(source + " --> " + destination);
+            System.out.println(intermediate + " --> " + destination);
+            return;
+        }
+        towerOfHanoi(source, destination, intermediate, num - 1);
+        System.out.println(source + " --> " + destination);
+        towerOfHanoi(intermediate, source, destination, num - 1);
+    }
+}
