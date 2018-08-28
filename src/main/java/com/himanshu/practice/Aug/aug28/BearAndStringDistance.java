1a362e3b45779b04e4b7fa55700c71cd587e16d0
Himanshu Bhardwaj <himanshubhardwaj169@gmail.com>
2018-08-29 01:15:20 +0530

    * Testing performance of PrintWriter and sout

--- src/main/java/com/himanshu/practice/Aug/aug28/BearAndStringDistance.java
+++ src/main/java/com/himanshu/practice/Aug/aug28/BearAndStringDistance.java
@@ -9,6 +9,7 @@ import java.io.PrintWriter;
  * Created by himanshubhardwaj on 29/08/18.
  * Statement: https://codeforces.com/contest/628/problem/C
  * Algo: Timepass
+ * Submission: https://codeforces.com/contest/628/problem/C
  */
 public class BearAndStringDistance {
     public static void main(String[] args) throws IOException {
