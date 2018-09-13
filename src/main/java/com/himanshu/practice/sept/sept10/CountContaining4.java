780d74dc34d0c08901653f89bc18751a7680c75d
Himanshu Bhardwaj <himanshubhardwaj169@gmail.com>
2018-09-13 13:37:34 +0530

    minor commits

---
+++ src/main/java/com/himanshu/practice/sept/sept10/CountContaining4.java
@@ -0,0 +1,44 @@
+package com.himanshu.practice.sept.sept10;
+
+/**
+ * Created by himanshubhardwaj on 11/09/18.
+ */
+public class CountContaining4 {
+    public static void main(String[] args) {
+        int count = 0;
+
+        for (int i = 0; i <= 10000000; i++) {
+            if (constains4(i)) {
+                count++;
+            }
+            if (i != 0) {
+                if (i == 10000000) {
+                    System.out.println(i + "\t" + count);
+                } else if (i == 1000000) {
+                    System.out.println(i + "\t" + count);
+                } else if (i == 100000) {
+                    System.out.println(i + "\t" + count);
+                } else if (i == 10000) {
+                    System.out.println(i + "\t" + count);
+                } else if (i == 1000) {
+                    System.out.println(i + "\t" + count);
+                } else if (i == 100) {
+                    System.out.println(i + "\t" + count);
+                } else if (i == 10) {
+                    System.out.println(i + "\t" + count);
+                }
+            }
+        }
+
+    }
+
+    static boolean constains4(int x) {
+        while (x > 0) {
+            if (x % 10 == 4) {
+                return true;
+            }
+            x = x / 10;
+        }
+        return false;
+    }
+}
