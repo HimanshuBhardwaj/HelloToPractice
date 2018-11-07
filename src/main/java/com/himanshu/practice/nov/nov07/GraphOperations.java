db5cb96dc91091ab6ab079835765c667c3c05ff3
Himanshu Bhardwaj <himanshubhardwaj169@gmail.com>
2018-11-07 20:24:40 +0530

    reversing linkedlist in pairs

---
+++ src/main/java/com/himanshu/practice/nov/nov07/GraphOperations.java
@@ -0,0 +1,11 @@
+package com.himanshu.practice.nov.nov07;
+
+/**
+ * Created by himanshubhardwaj on 07/11/18.
+ */
+
+/*
+* DFS, BFS,
+* */
+public class GraphOperations {
+}
---
+++ src/main/java/com/himanshu/practice/nov/nov07/TreeOperations.java
@@ -0,0 +1,10 @@
+package com.himanshu.practice.nov.nov07;
+
+/**
+ * Created by himanshubhardwaj on 07/11/18.
+ */
+/*
+* Linking nodes of same level. diameter of tree
+* */
+public class TreeOperations {
+}
