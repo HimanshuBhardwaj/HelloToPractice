b91802ef88aec71535a64cc9d0aec7d29e55192c
Himanshu Bhardwaj <himanshubhardwaj169@gmail.com>
2019-04-09 22:04:04 +0530

    irst commit

---
+++ src/main/java/com/himanshu/practice/y2019/April/april6/C.java
@@ -0,0 +1,15 @@
+package com.himanshu.practice.y2019.April.april6;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by himanshubhardwaj on 06/04/19.
+ */
+public class C {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
