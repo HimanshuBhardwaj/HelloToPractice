750467e7b933f913ac323f747912a0d9e0be585c
Himanshu Bhardwaj <himanshubhardwaj169@gmail.com>
2019-04-23 17:51:56 +0530

    minor changes

---
+++ src/main/java/com/himanshu/practice/y2019/April/april19/RangeMinimumQuery.java
@@ -0,0 +1,17 @@
+package com.himanshu.practice.y2019.April.april19;
+
+/**
+ * Created by himanshubhardwaj on 19/04/19.
+ *
+ */
+public class RangeMinimumQuery {
+}
+
+
+class SegmentTree {
+    int [] tree;
+    int size;
+
+ //   public SegmentTree()
+
+}
